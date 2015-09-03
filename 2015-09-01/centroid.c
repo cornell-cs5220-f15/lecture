8120b0a50d53edb707a5f2c472996947fc144e4f
David Bindel <bindel@cs.cornell.edu>
2015-09-03 13:05:35 -0400

    Update centroid example

--- 2015-09-01/centroid.c
+++ 2015-09-01/centroid.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "centroid.h"
 
 void test_mean1(double *xymean, double* xy, int n)
 {
@@ -41,36 +39,3 @@ void test_mean3(double *xymean, double* xy, int n)
     xymean[1] = ysum/n;
 }
 
-int main()
-{
-    int i;
-    int N = 1000000;
-/*    int trials = 100; */
-    int trials = 50; 
-    int tstart, tstop;
-    double xymean[2];
-    double* xy = (double*) malloc(2 * N * sizeof(double));
-
-    for (i = 0; i < 2*N; ++i)
-        xy[i] = random();
-
-    tstart = clock();
-    for (i = 0; i < trials; ++i)
-        test_mean1(xymean, xy, N);
-    tstop = clock();
-    printf("Version 1: %e\n", (double) (tstop-tstart)/trials/CLOCKS_PER_SEC);
-
-    tstart = clock();
-    for (i = 0; i < trials; ++i)
-        test_mean2(xymean, xy, N);
-    tstop = clock();
-    printf("Version 2: %e\n", (double) (tstop-tstart)/trials/CLOCKS_PER_SEC);
-
-    tstart = clock();
-    for (i = 0; i < trials; ++i)
-        test_mean3(xymean, xy, N);
-    tstop = clock();
-    printf("Version 3: %e\n", (double) (tstop-tstart)/trials/CLOCKS_PER_SEC);
-
-    free(xy);
-}
---
+++ 2015-09-01/centroid.h
@@ -0,0 +1,8 @@
+#ifndef CENTROID_H
+#define CENTROID_H
+
+void test_mean1(double *xymean, double* xy, int n);
+void test_mean2(double *xymean, double* xy, int n);
+void test_mean3(double *xymean, double* xy, int n);
+
+#endif /* CENTROID_H */

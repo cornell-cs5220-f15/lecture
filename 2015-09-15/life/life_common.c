b46bd944e64003be40722c787a5e1eed75068354
David Bindel <bindel@cs.cornell.edu>
2015-09-15 13:15:35 -0400

    Update Game of Life build (timing + enhanced Makefile).

--- 2015-09-15/life/life_common.c
+++ 2015-09-15/life/life_common.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef WITH_TIMING
+#include <omp.h>
+#endif
+
 #include "crc32.h"
 #include "life_common.h"
 
@@ -124,8 +128,17 @@ int main(int argc, char** argv)
             print_board(&problem);
             advance_board(&problem, 1);
         }
-    } else
+    } else {
+#ifdef WITH_TIMING
+        double t0 = omp_get_wtime();
         advance_board(&problem, problem.g);
+        double t1 = omp_get_wtime();
+        printf("Cells / sec: %e\n",
+               problem.g * problem.nboard * problem.nboard / (t1-t0));
+#else
+        advance_board(&problem, problem.g);
+#endif
+    }
     printf("Final checksum: %08X\n", board_checksum(&problem));
     destroy_board(&problem);
     return 0;
